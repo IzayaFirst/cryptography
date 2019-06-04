4deb6573cb203e0fd429484bac9c084b146847a7
First Patchara <patcharaleelalumplert@gmail.com>
2019-06-04 18:13:37 +0700

    add go routine

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/IzayaFirst/cryptography/cipher"
 )
@@ -22,33 +24,9 @@ type Charging struct {
 	ExpYear  int     `json:"expire_year"`
 }
 
-func main() {
-	cipherText, err := os.Open("data/fng.1000.csv.rot128")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cipherText.Close()
-
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(cipherText)
-	decryption, errDecryption := cipher.NewRot128Reader(buf)
-	if errDecryption != nil {
-		log.Fatal(errDecryption)
-	}
-	bufReader := make([]byte, 1)
-	planTextBuff := new(bytes.Buffer)
-	for {
-		_, errReadingDecryption := decryption.Read(bufReader)
-		if errReadingDecryption == io.EOF {
-			break
-		} else {
-			planTextBuff.Write(bufReader)
-		}
-	}
-	plainText := planTextBuff.String()
-	scanner := bufio.NewScanner(strings.NewReader(plainText))
+func convertToArray(scanner *bufio.Scanner, charges chan []Charging, wg *sync.WaitGroup) {
 	rowNum := 0
-	charges := make([]Charging, 0)
+	charger := make([]Charging, 0)
 	for scanner.Scan() {
 		rowNum++
 		if rowNum == 1 {
@@ -72,7 +50,55 @@ func main() {
 				ExpMonth: expMonth,
 				ExpYear:  expYear,
 			}
-			charges = append(charges, *charge)
+			charger = append(charger, *charge)
 		}
 	}
+	charges <- charger
+	close(charges)
+	wg.Done()
+}
+
+func main() {
+	cipherText, err := os.Open("data/fng.1000.csv.rot128")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cipherText.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(cipherText)
+	decryption, errDecryption := cipher.NewRot128Reader(buf)
+	if errDecryption != nil {
+		log.Fatal(errDecryption)
+	}
+	ch := make(chan string)
+	go readBufferFile(decryption, ch)
+	var wg sync.WaitGroup
+	charges := make(chan []Charging)
+	for c := range ch {
+		wg.Add(1)
+		scanner := bufio.NewScanner(strings.NewReader(c))
+		go convertToArray(scanner, charges, &wg)
+	}
+	for c := range charges {
+		fmt.Print(c)
+		fmt.Print(len(c))
+	}
+	wg.Wait()
+}
+
+func readBufferFile(cipherTextBuffer *cipher.Rot128Reader, c chan string) {
+	bufReader := make([]byte, 1)
+	planTextBuff := new(bytes.Buffer)
+	for {
+		_, errReadingDecryption := cipherTextBuffer.Read(bufReader)
+		if errReadingDecryption == io.EOF {
+			break
+		} else {
+			planTextBuff.Write(bufReader)
+		}
+	}
+	plainText := planTextBuff.String()
+	c <- plainText
+	close(c)
 }
